337838f6ec223df2546f64cbf863f71de1588822
JessCP25 <jpayanoc25@gmail.com>
2025-07-20 11:08:31 -0500

    Feat: Usando useRef para input

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 // import noResults from './mocks/no-results.json;'
+import { useRef } from "react";
 import "./App.css";
 import { Movies } from "./components/Movies";
 import { useMovie } from "./hooks/useMovie";
@@ -6,13 +7,23 @@ import { useMovie } from "./hooks/useMovie";
 function App() {
   const { movies } = useMovie()
 
+  const inputRef = useRef();
+
+  const handleSubmit = (event)=> {
+    event.preventDefault();
+    const inputEl = inputRef.current;
+    const value = inputEl.value;
+
+    console.log({value});
+  }
+
   return (
     <>
       <div className="page">
         <header>
           <h1>Buscador de películas</h1>
-          <form>
-            <input placeholder="Avengers, Matrix, Harry Potter, ..." />
+          <form onSubmit={handleSubmit}>
+            <input name="query" ref={inputRef} placeholder="Avengers, Matrix, Harry Potter, ..." />
             <button type="submit">Buscar</button>
           </form>
         </header>
