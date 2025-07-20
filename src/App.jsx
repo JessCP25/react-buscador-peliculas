e7bbcfa548d03e2f64bf83a4d5059f1da75e3e38
JessCP25 <jpayanoc25@gmail.com>
2025-07-19 21:28:44 -0500

    Feat: Renderizando peliculas

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import responseMovies from './mocks/with-results.json';
+// import noResults from './mocks/no-results.json;'
+import './App.css'
+
+function App() {
+
+  const movies =responseMovies.Search;
+
+  const hasMovies = movies.length > 0
+
+  return (
+    <>
+      <div className='page'>
+        <header>
+        <h1>Buscador de películas</h1>
+        <form>
+          <input placeholder='Avengers, Matrix, Harry Potter, ...' />
+          <button type='submit'>Buscar</button>
+        </form>
+        </header>
+        <main>
+            {
+              hasMovies ? (
+                <ul>  
+                  {
+                    movies.map(movie => <li key={movie.imdbID}>
+                      <h3>{movie.Title}</h3>
+                      <p>{movie.Year}</p>
+                      <img src={movie.Poster} alt={movie.Title} />
+                    </li>)
+                  }
+          </ul>
+              )
+              : (
+                <p>No se encontraron películas con las busqueda</p>
+              )
+            }
+        </main>
+      </div>
+    </>
+  )
+}
+
+export default App
